98ddd79c71c27df7208b31d2eb0f4a926737a70b
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-03-04 17:56:45 +0000

    Adds tonerAmount property to Printer class, tested with JUnit

--- src/main/java/Printer.kt
+++ src/main/java/Printer.kt
@@ -1,12 +1,17 @@
-class Printer(numberOfSheets: Int) {
+class Printer(numberOfSheets: Int, tonerAmount: Int) {
 
     var numberOfSheets: Int = numberOfSheets
+    var tonerAmount: Int = tonerAmount
 
     fun print(numberOfPages: Int, numberOfCopies: Int): String {
         val pagesUsed: Int = numberOfPages * numberOfCopies
+        if (pagesUsed > tonerAmount) {
+            return "Not enough toner!"
+        }
         when (pagesUsed <= numberOfSheets) {
             true -> {
                 numberOfSheets -= pagesUsed
+                tonerAmount -= pagesUsed
                 return "Printed $numberOfCopies copies with $numberOfPages pages each"
             }
             false -> return "Not enough sheets!"
--- src/test/java/PrinterTest.kt
+++ src/test/java/PrinterTest.kt
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class PrinterTest {
 
-    val printer = Printer(10)
+    val printer = Printer(10, 10)
 
     @Test
     fun getNumberOfSheets() {
         assertEquals(10, printer.numberOfSheets)
+    }
+
+    @Test
+    fun getTonerAmount() {
+        assertEquals(10, printer.tonerAmount)
     }
 
     @Test
@@ -18,15 +23,30 @@ internal class PrinterTest {
     }
 
     @Test
+    fun setTonerAmount() {
+        printer.tonerAmount = 20
+        assertEquals(20, printer.tonerAmount)
+    }
+
+    @Test
     fun print() {
         val output: String = printer.print(4, 2)
         assertEquals("Printed 2 copies with 4 pages each", output)
         assertEquals(2, printer.numberOfSheets)
+        assertEquals(2, printer.tonerAmount)
     }
 
     @Test
     fun notEnoughSheets() {
-        val output: String = printer.print(5, 3)
+        printer.numberOfSheets = 4
+        val output: String = printer.print(4, 2)
         assertEquals("Not enough sheets!", output)
+    }
+
+    @Test
+    fun notEnoughToner() {
+        printer.tonerAmount = 5
+        val output: String = printer.print(4, 2)
+        assertEquals("Not enough toner!", output)
     }
 }
